32b4dbd60c80dbf19b16d4b29d4cd2b1aeba9f0a
RedKyten <dhostett@ualberta.ca>
2015-01-24 08:30:52 -0700

    Fixed the grapple

--- GGJ2015/Assets/Scripts/Activate.cs
+++ GGJ2015/Assets/Scripts/Activate.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -13,13 +13,18 @@ public class Activate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (gameObject.GetComponent<SpringJoint2D> ().connectedBody != null){
+			if (GameObject.Find("Player").transform.position.x >= (this.transform.position.x-.3) && GameObject.Find("Player").transform.position.x <= (this.transform.position.x+.3) ){
+				gameObject.GetComponent<SpringJoint2D> ().connectedBody = null;	
+			}
+		}
 	}
 
 	void OnMouseDown(){
-		//activeposition = this.transform.position;
-       // Debug.Log("sent");
-        GameObject.Find ("Player").SendMessage("Grapple",this.gameObject );
-		//Debug.Log ("sent");
-
+		Debug.Log(gameObject.GetComponent<SpringJoint2D> ().connectedBody);
+		float dist = Vector2.Distance (GameObject.Find ("Player").transform.position, this.transform.position);
+		if (dist <= 10f){
+			gameObject.GetComponent<SpringJoint2D> ().connectedBody = GameObject.Find("Player").rigidbody2D;
+		}
 	}
 }
--- GGJ2015/Assets/Scripts/Character.cs
+++ GGJ2015/Assets/Scripts/Character.cs
@@ -24,18 +24,7 @@ public class Character : MonoBehaviour {
 			position.x++;
 			this.transform.position = position;
 				}
-	if (active != null){
-		if (active.transform.position.x == this.transform.position.x){
-			gameObject.GetComponent<SpringJoint> ().connectedBody = null;	
-			}
-		}
-	}
-	void Grapple(GameObject end){
-		Debug.Log (end.transform.position.x);
-		gameObject.GetComponent<SpringJoint2D> ().connectedBody = end.rigidbody2D;
-		active = end;
-
-//			this.transform.position = Vector2.Lerp (this.transform.position, end, step);
 
 	}
+
 }
