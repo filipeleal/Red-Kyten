ed2076ae6a5d3f66ab716d685dc416f2ce02860c
RedKyten <dhostett@ualberta.ca>
2015-01-24 00:06:58 -0700

    My work

---
+++ GGJ2015/Activate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Activate : MonoBehaviour {
+	Vector2 activeposition;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseDown(){
+		activeposition = this.transform.position;
+		GameObject.Find ("Player").SendMessage("Grapple",this.gameObject );
+		Debug.Log ("sent");
+
+	}
+}
---
+++ GGJ2015/Character.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character : MonoBehaviour {
+	public float speed;
+	static float step;
+	GameObject active;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		step = speed * Time.deltaTime;
+	if (Input.GetKey(KeyCode.A)) {
+			Vector2 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+				}
+	if (Input.GetKey(KeyCode.D)) {
+			Vector2 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+				}
+	if (active != null){
+		if (active.transform.position.x == this.transform.position.x){
+			gameObject.GetComponent<SpringJoint> ().connectedBody = null;	
+			}
+		}
+	}
+	void Grapple(GameObject end){
+		Debug.Log (end.transform.position.x);
+		gameObject.GetComponent<SpringJoint2D> ().connectedBody = end.rigidbody2D;
+		active = end;
+
+//			this.transform.position = Vector2.Lerp (this.transform.position, end, step);
+
+	}
+}
