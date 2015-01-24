1b531a17e0a4acd177525ac82fb853117dabf2e2
RedKyten <dhostett@ualberta.ca>
2015-01-24 10:01:57 -0700

    Test

--- GGJ2015/Assets/Scripts/Character.cs
+++ GGJ2015/Assets/Scripts/Character.cs
@@ -6,6 +6,8 @@ public class Character : MonoBehaviour {
 	public float speed;
 	static float step;
 	GameObject active;
+	public Rigidbody2D minionsPrefab;
+	public Transform minionsLaunch;
 	// Use this for initialization
 	void Start () {
 
@@ -14,17 +16,20 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		step = speed * Time.deltaTime;
-	if (Input.GetKey(KeyCode.A)) {
+		if (Input.GetKey(KeyCode.A)) {
 			Vector2 position = this.transform.position;
 			position.x--;
 			this.transform.position = position;
 				}
-	if (Input.GetKey(KeyCode.D)) {
+		if (Input.GetKey(KeyCode.D)) {
 			Vector2 position = this.transform.position;
 			position.x++;
 			this.transform.position = position;
 				}
-
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Rigidbody2D minion = Instantiate(minionsPrefab, minionsLaunch.position, minionsLaunch.rotation ) as Rigidbody2D;
+			minion.AddForce(minionsLaunch.forward * 500);
+		}
 	}
 
 }
