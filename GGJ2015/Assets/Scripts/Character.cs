a4880cf564efbb13bed643ea31c65f2e3e3e4a5f
filipeleal <filipe.sama@gmail.com>
2015-01-24 14:53:05 -0700

    Flip

--- GGJ2015/Assets/Scripts/Character.cs
+++ GGJ2015/Assets/Scripts/Character.cs
@@ -20,22 +20,27 @@ public class Character : MonoBehaviour {
 		if (move != 0){
 			rigidbody2D.velocity = new Vector2 (move * speed, rigidbody2D.velocity.y);
 		}
-		if (move > 0 && !facingRight){
+
+        var mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		if (( mousePos.x > transform.localPosition.x) && !facingRight){
 			Flip ();
 		}
-		else if(move <0 && facingRight){
+        else if (( mousePos.x < transform.localPosition.x) && facingRight)
+        {
 			Flip();
 		}
 	}
 	void Update () {
 
 		if (Input.GetKeyDown (KeyCode.Mouse0) && Activate.active == false) {
-			Rigidbody2D minion = Instantiate(minionsPrefab, minionsLaunch.position, minionsLaunch.rotation ) as Rigidbody2D;
-			Debug.Log (minionsLaunch.forward);
-            //minion.AddForce(Input.mousePosition.normalized * -5000f);
-
+			
+			
             var mousePos =  Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            
+            Rigidbody2D minion = Instantiate(minionsPrefab, minionsLaunch.position, minionsLaunch.rotation) as Rigidbody2D;
 
             minion.transform.LookAt(mousePos);
             minion.AddForce(minion.transform.forward * 5000f);
