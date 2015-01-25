e1ea3d65ac2291352b7ad2c25a4f4461209911a2
RedKyten <dhostett@ualberta.ca>
2015-01-25 10:37:24 -0700

    help

--- GGJ2015/Assets/Scripts/Minionscript.cs
+++ GGJ2015/Assets/Scripts/Minionscript.cs
@@ -13,13 +13,13 @@ public class Minionscript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float step = speed * Time.deltaTime;
-        Vector2 place = new Vector2((GameObject.FindGameObjectWithTag("Player").transform.position.x - 2), (GameObject.FindGameObjectWithTag("Player").transform.position.y - 1.4f));
+        Vector2 place = new Vector2((GameObject.FindGameObjectWithTag("Player").transform.position.x - 2), (GameObject.FindGameObjectWithTag("Player").transform.position.y - 1.1f));
 		if (Character.facingRight == true){
 			gameObject.transform.position = Vector2.MoveTowards (gameObject.transform.position, place, step );
            
 		}
 		else{
-            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, new Vector2((GameObject.FindGameObjectWithTag("Player").transform.position.x + 2), (GameObject.FindGameObjectWithTag("Player").transform.position.y) - 1.4f), step);
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, new Vector2((GameObject.FindGameObjectWithTag("Player").transform.position.x + 2), (GameObject.FindGameObjectWithTag("Player").transform.position.y) - 1.1f), step);
 
 		}
 
