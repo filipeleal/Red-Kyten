2de34606bf8bd9fec3e127caab9704ecc5e7346f
RedKyten <dhostett@ualberta.ca>
2015-01-24 20:00:08 -0700

    Level switch included

---
+++ GGJ2015/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.Log (col.gameObject.name);
+		if (col.gameObject.name == "Player"){
+			GameObject.Find ("Player").SendMessage("Death",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
